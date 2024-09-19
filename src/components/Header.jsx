9fd9d2287b78f5e735cd9811129f1b9f2b5d0407
Ömer Tanrıverdi <omer.f.tanriverdi@gmail.com>
2024-09-19 20:55:38 +0300

    bazı updateler

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import { useHistory } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import { useHistory } from "react-router-dom";
+import { Button } from "reactstrap";
 
 function Header(props) {
   const history = useHistory();
 
   return (
     <header>
-      <h1>WiStore Products</h1>
-      <Button color="primary" onClick={() => history.push('/')}>
+      <h1>My WiStore Products</h1>
+      <Button color="primary" onClick={() => history.push("/")}>
         Login
       </Button>
     </header>
